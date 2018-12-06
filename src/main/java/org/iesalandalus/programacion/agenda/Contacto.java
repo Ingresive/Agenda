c8834d4e24b53a3202e289d440e64ffbbdff1bbd
Jose <Jose@Jose-PC>
2018-12-06 01:00:27 +0100

    Ahora si, añadidos los atributos

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -1,5 +1,12 @@
 package org.iesalandalus.programacion.agenda;
 
 public class Contacto {
+	
+	private static final String ER_TELEFONO = "sdfsd";
+	private static final String ER_CORREO = "asda";
+
+	private String nombre;
+	private String telefono;
+	private String correo;
 
 }
