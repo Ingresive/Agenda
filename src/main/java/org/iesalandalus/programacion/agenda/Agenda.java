5a7054bd5397425a48844dc1da33bff22e860387
Jose <Jose@Jose-PC>
2018-12-09 19:00:04 +0100

    Añadidos métodos get para Clase Agenda.
    Creado array "contactos".
    
    Creado el constructor e implementado para que devuelva una copia del
    array anterior.

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,5 +5,31 @@ public class Agenda {
 	private static final int MAX_CONTACTOS = 50;
 	
 	private int numContactos;
+	
+	private Contacto[] contactos ;
+	
+	/*  CONSTRUCTOR  */
+	
+	public Agenda () {
+		
+		contactos = new Contacto[MAX_CONTACTOS];
+		
+	}
+	
+	
+	/*    MÉTODOS GET   */
+	
+	public Contacto[] getContactos() {
+		
+		return contactos;
+		
+			
+	}
 
+	public int getNumContactos () {
+		
+		return numContactos;
+	}
+	
+	
 }
